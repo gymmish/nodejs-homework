8240d16c80ee43365933af740d7ff6eb1c211de2
AS <samaloanastasia@gmail.com>
2023-06-13 22:59:14 +0300

    ,

---
+++ routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const { validateBody, authenticate } = require("../../utils");
+const {registJoiSchema, loginJoiSchema} = require("../../models/users");
+const ctrl = require("../../controllers/auth")
+
+router.post("/register", validateBody(registJoiSchema), ctrl.register );
+
+router.post("/login", validateBody(loginJoiSchema), ctrl.login );
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
+
+
+module.exports = router;
